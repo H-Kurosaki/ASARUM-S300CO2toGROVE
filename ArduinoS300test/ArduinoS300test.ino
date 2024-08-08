940e7f1a7ab58132e834514ce2f755ba67526010
Hideto KUROSAKI <50898673+H-Kurosaki@users.noreply.github.com>
2024-08-08 15:01:46 +0900

    Update ArduinoS300test.ino

--- ArduinoS300test/ArduinoS300test.ino
+++ ArduinoS300test/ArduinoS300test.ino
@@ -1,1 +1,22 @@
+//for Arduino UNO or MEGA R3
+//S-300 I2C test program
+//s300i2c.h can get from https://github.com/mhorimoto/ELT_S300_HOLLY
+#include "s300i2c.h"
+#include <Wire.h>
+S300I2C s3(Wire);
 
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  s3.begin(S300I2C_ADDR);
+  delay(3000);
+  Serial.println("START S300I2C");
+}
+
+void loop() {
+  unsigned int co2;
+  co2 = s3.getCO2ppm();
+  Serial.print("CO2=");
+  Serial.println(co2);
+  delay(3000);
+}
